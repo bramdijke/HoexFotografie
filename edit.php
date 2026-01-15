07ce81f72630760bef494a5b9cd6b9734138d15d
IbsenE10 <23047364@student.hhs.nl>
2026-01-15 14:57:27 +0100

    Create edit.php

---
+++ edit.php
@@ -0,0 +1,50 @@
+<?php
+/*include 'database.php';
+session_start();*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>Document</title>
+  <link rel="stylesheet" href="stylesheet.css">
+</head>
+    <body>
+
+    <nav class="sticky-top">
+    <p>HOEX</p>
+    <ul>
+        <li><a href="edit.php">Portfolio</a></li>
+        <li><a href="about.php">Reserveringen</a></li>
+        <li><a href="index.php">Over</a></li>
+    </ul>
+    </nav>
+
+    <div class="header-container">
+  <header>
+      <h1>[Categorie] bijwerken</h1>
+      <button>Toevoegen</button>
+  </header >
+    </div>
+  <main>
+    <ul>
+        <li>
+            <img src="/images/wedding.webp" alt="Weddings">
+            <h3>Weddings</h3>
+            <p>I document your wedding day with an eye for genuine emotions, meaningful details, and natural
+                moments. From intimate glances to grand celebrations, every image tells the story of your love in a
+                timeless and elegant way.</p>
+            <button>Bewerken</button>
+            <button>Verwijderen</button>
+    </ul>
+  </main>
+
+  <footer>
+    <p>&copy; 2026</p>
+  </footer>
+
+</body>
+</html>
